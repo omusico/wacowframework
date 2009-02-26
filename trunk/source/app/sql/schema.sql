5dc4b84558ccf4f479f58af60cf8dfdf71bb7cd1
jaceju <jaceju@a75dea25-5f38-0410-a777-75cbac79620a>
2009-02-26 08:41:55 +0000

    rebuild

--- trunk/source/app/sql/schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `empty`;
-CREATE TABLE `empty` (
-  `id` int(10) unsigned NOT NULL auto_increment,
-  `status` enum('y','n','x') NOT NULL default 'y',
-  `createdDateTime` datetime default NULL,
-  `updatedDateTime` datetime default NULL,
-  PRIMARY KEY  (`id`),
-  KEY `status` (`status`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
