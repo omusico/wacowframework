351154ce28f920a6f2cff2213d14f0b86a8eee34
jaceju@gmail.com <jaceju@gmail.com@a75dea25-5f38-0410-a777-75cbac79620a>
2011-01-27 07:17:50 +0000

    prepare for 3.0

--- trunk/source/pub/js/admin/menu.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $('#menu > li > ul').hide();
-    $('#menu > li').hover(
-        function () { $('ul', this).slideDown('fast'); },
-        function () { var $menu = this; setTimeout(function () { $('ul', $menu).slideUp('fast'); }, 300) }
-    );
-});
